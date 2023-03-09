fd9e27991851e8f2b2865d139519c1002c8dee5a
burak cevizli <burak.cevizli7@gmail.com>
2023-03-09 03:05:13 +0300

    Slider

---
+++ Foodie/Models/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  Foodie
+//
+//  Created by Adem Burak Cevizli on 8.03.2023.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage?
+}
---
+++ Foodie/Views/Onboarding/OnboardingCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  Foodie
+//
+//  Created by Adem Burak Cevizli on 8.03.2023.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = String(describing: OnboardingCollectionViewCell.self)
+    
+    @IBOutlet weak var slideImageView: UIImageView!
+    
+    @IBOutlet weak var slideTitleLabel: UILabel!
+    
+    @IBOutlet weak var slideDescriptionLabel: UILabel!
+    
+    func setup(_ slide: OnboardingSlide) {
+        slideImageView.image = slide.image
+        slideTitleLabel.text = slide.title
+        slideDescriptionLabel.text = slide.description
+    }
+    
+}
+
